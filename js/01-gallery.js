3ab7c605cd94f6d1cce042c27de4782c72801583
maxim <103951335+Makmar1983@users.noreply.github.com>
2022-09-21 13:50:58 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ref = {
+    containerForGallery: document.querySelector('.gallery'),
+}
+let galleryItem = '';
+galleryItems.map((key)=>{
+     galleryItem += `
+        <div class="gallery__item">
+        <a class="gallery__link" href="#">
+                <img
+                    class="gallery__image"
+                    src="${key.preview}"
+                    data-source="${key.original}"
+                    alt="${key.description}"
+                />
+                </a>
+        </div>`;    
+})
+
+ref.containerForGallery.insertAdjacentHTML("afterbegin", galleryItem)
+
+ref.containerForGallery.addEventListener(`click`,openModalWindow)
+function openModalWindow (event) {
+    event.preventDefault();
+	const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    </div>
+	`);
+    instance.show();
+    
+    let imgPicture = document.querySelector('.modal');
+    imgPicture.addEventListener('click', closeModalWindow);
+
+    function closeModalWindow(event){
+        event.preventDefault();
+        instance.close();
+        window.removeEventListener('keydown', usingEsc);
+        window.removeEventListener('click', closeModalWindow);
+    }
+
+    window.addEventListener('keydown', usingEsc);
+    function usingEsc(e){
+        if(!basicLightbox.visible()){return};
+        if(e.key=='Escape'||e.key=='Esc'){
+            e.preventDefault();
+            instance.close();
+            window.removeEventListener('keydown', usingEsc);
+            window.removeEventListener('click', closeModalWindow);
+        }
+}}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
+
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ref = {
+    containerForGallery: document.querySelector('.gallery'),
+}
+let galleryItem = '';
+const buildGallery = galleryItems.map(({original,preview, description})=>{
+     galleryItem += `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+                </a>
+        </div>`;    
+})
+
+ref.containerForGallery.innerHTML = galleryItem;
+new SimpleLightbox('.gallery__link', { 
+    captionsData: "alt",  
+    captionDelay: 250, 
+
+ });
